f89d19c348cf23dfbb45dd3248e7e860076f9d47
Swizzyfr <nukediscordzel@gmail.com>
2025-01-19 08:08:57 +1100

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -1,25 +1,42 @@
 import socket
+import threading
+import sys
 
-# Replace with the correct server IP address
-SERVER_IP = "192.168.88.238"  # Use the server's local IP address
-PORT = 5555
+# This will allow the client to connect to a specific IP address and port
+server_ip = '127.0.0.1'  # Change this if you're using a different IP
+server_port = 5555
 
+# Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server using the local IP address
-client_socket.connect((SERVER_IP, PORT))
+# Connect to the server
+client_socket.connect((server_ip, server_port))
 
-# Get the username of the device running the client
-username = input("Enter your username: ")
+# Function to receive messages from the server
+def receive_messages():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')  # Receiving data from the server
+            if message:
+                print(message)  # Print message from server
+            else:
+                break
+        except:
+            print("Error receiving message.")
+            break
 
-# Send the username to the server
-client_socket.send(username.encode('utf-8'))
+# Function to send messages to the server
+def send_messages():
+    while True:
+        message = input("You: ")  # Input from the user
+        if message.lower() == 'exit':
+            client_socket.send("User has left the chat.".encode('utf-8'))
+            break
+        client_socket.send(f"User: {message}".encode('utf-8'))  # Send message to server
 
-# Receive and send messages
-while True:
-    message = input(f"{username}: ")
-    if message.lower() == 'exit':
-        break
-    client_socket.send(message.encode('utf-8'))
+# Start threads for receiving and sending messages
+thread_receive = threading.Thread(target=receive_messages)
+thread_receive.start()
 
-client_socket.close()
+thread_send = threading.Thread(target=send_messages)
+thread_send.start()
